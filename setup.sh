863cfb64f509108e74f4ba91d1dc77bf7d5e9a6d
pablogarin <pablo.garin@hotmail.com>
2016-09-02 12:26:59 -0300

    auto generated command on /usr/bin/ for global usage

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 apt-get install deluged deluge-console python-dev python-pip
 pip install feedparser xmltodict
+touch /usr/bin/torrentmanager
+echo '#!/bin/bash' > /usr/bin/torrentmanager
+echo "$(pwd)/main.py \$@" >> /usr/bin/torrentmanager
