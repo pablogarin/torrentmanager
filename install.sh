62322e97b45e352b524a0f31feaa7e20e369b37c
pablogarin <pablo.garin@hotmail.com>
2016-09-02 15:17:06 -0300

    added lxml to install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install deluged deluge-console python-dev python-pip
-pip install feedparser xmltodict
+pip install feedparser xmltodict lxml
 touch /usr/bin/torrentmanager
 echo '#!/bin/bash' > /usr/bin/torrentmanager
 echo "$(pwd)/main.py \$@" >> /usr/bin/torrentmanager
