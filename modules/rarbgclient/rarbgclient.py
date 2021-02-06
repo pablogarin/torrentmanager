5b122671385e33568c3ee5c5eb783df156a1f34c
pablo <pablo.garin@hotmail.com>
2021-02-06 11:30:22 -0300

    Feat: created rarbg torrent client

---
+++ modules/rarbgclient/rarbgclient.py
@@ -0,0 +1,27 @@
+import feedparser
+from modules.interfaces import FeedClientInterface
+from modules.interfaces import FeedEntry
+from modules.interfaces import FeedList
+
+
+class RarbgClient(FeedClientInterface):
+    _url = "http://rarbg.to/rssdd.php?category=1;18;41;49"
+
+    def __init__(self):
+        pass
+
+    def read(self) -> FeedList:
+        try:
+            feed = feedparser.parse(self._url)
+            feedentries = feed['entries']
+            feed = None
+            return list(map(
+                self._create_feed_entry,
+                feedentries))
+        except Exception as e:
+            print("Error while reading the feed: %s" % e)
+
+    def _create_feed_entry(self, entry: list) -> FeedEntry:
+        title = entry['title']
+        link = entry['link']
+        return FeedEntry(title, link)
