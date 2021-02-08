5711847482eaca4a6b1786648761b72714f9cd6a
pablo <pablo.garin@hotmail.com>
2021-02-08 16:39:25 -0300

    Fix: added try to handle keyboard interrupt

--- torrentmanager/torrentmanager.py
+++ torrentmanager/torrentmanager.py
@@ -54,57 +54,64 @@ def _get_arguments() -> dict:
 
 
 def main():
-    args = _get_arguments()
-    if len(args) > 1:
-        raise Exception('too many arguments')
-    # Config
-    config = Config()
-    torrent_folder = config.get_config("download_folder")
-    torrent_quality = config.get_config("quality")
-    # Database
-    show_manager = ShowManager()
-    # Show Info Client
-    tvdb = TVDBClient(show_manager)
-    # Workers
-    show_finder = ShowFinder(
-        show_manager,
-        tvdb,
-        torrent_folder)
-    torrent_finder = TorrentFinder(
-        show_manager,
-        torrent_folder,
-        torrent_quality)
-    # Check Args
-    if len(args) == 0:
-        torrent_finder.read_rss(RarbgClient())
-    elif 'update' in args and args['update']:
-        for show in show_manager.find():
-            seriesid = show.thetvdbID
-            print("Updating show '%s'(%s)" % (show.title, show.thetvdbID))
-            if seriesid is not None:
-                show_finder.schedule_show(seriesid, update=True)
-    elif 'by_name' in args and args['by_name']:
-        torrent_finder.check_scheduled_shows(EZTVClient())
-    elif 'list' in args and args['list']:
-        for show in show_manager.find():
-            print(show)
-    elif 'config' in args and args['config']:
-        config.initial_configuration()
-    elif 'search' in args:
-        if len(args['search']) > 0:
-            query = " ".join(args['search'])
-            shows = show_finder.search(query)
-            if len(shows) == 0:
-                print("No Matches for Show")
-        else:
-            show_finder.interactive_search()
-    elif 'add' in args:
-        for show_id in args['add']:
-            print(show_id)
-            if show_finder.schedule_show(show_id):
-                print("Show Added")
+    try:
+        args = _get_arguments()
+        if len(args) > 1:
+            raise Exception('too many arguments')
+        # Config
+        config = Config()
+        torrent_folder = config.get_config("download_folder")
+        torrent_quality = config.get_config("quality")
+        # Database
+        show_manager = ShowManager()
+        # Show Info Client
+        tvdb = TVDBClient(show_manager)
+        # Workers
+        show_finder = ShowFinder(
+            show_manager,
+            tvdb,
+            torrent_folder)
+        torrent_finder = TorrentFinder(
+            show_manager,
+            torrent_folder,
+            torrent_quality)
+        # Check Args
+        if len(args) == 0:
+            torrent_finder.read_rss(RarbgClient())
+        elif 'update' in args and args['update']:
+            for show in show_manager.find():
+                seriesid = show.thetvdbID
+                print("Updating show '%s'(%s)" % (show.title, show.thetvdbID))
+                if seriesid is not None:
+                    show_finder.schedule_show(seriesid, update=True)
+        elif 'by_name' in args and args['by_name']:
+            torrent_finder.check_scheduled_shows(EZTVClient())
+        elif 'list' in args and args['list']:
+            for show in show_manager.find():
+                print(show)
+        elif 'config' in args and args['config']:
+            config.initial_configuration()
+        elif 'search' in args:
+            if len(args['search']) > 0:
+                query = " ".join(args['search'])
+                shows = show_finder.search(query)
+                if len(shows) == 0:
+                    print("No Matches for Show")
             else:
-                print("Show not found!")
-    else:
-        print("¯\\_(ツ)_/¯")
-    return 0
+                show_finder.interactive_search()
+        elif 'add' in args:
+            for show_id in args['add']:
+                print(show_id)
+                if show_finder.schedule_show(show_id):
+                    print("Show Added")
+                else:
+                    print("Show not found!")
+        else:
+            print("¯\\_(ツ)_/¯")
+        return 0
+    except KeyboardInterrupt:
+        print("Cancelled by User")
+        return 1
+    except Exception as e:
+        print("Program halted: %s" % e)
+        return 2
