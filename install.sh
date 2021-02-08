b4df1845aed08c1d541f6c53978be41cca9d8f26
pablo <pablo.garin@hotmail.com>
2021-02-08 00:52:43 -0300

    Fix: install file had reference to python2

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get -qy install deluged deluge-console python-dev python-pip libxml2-dev libxslt-dev
+apt-get -qy install deluged deluge-console python-dev python3-pip libxml2-dev libxslt-dev
 pip install -r requirements.txt --quiet
 touch /usr/bin/torrentmanager
 echo '#!/bin/bash' > /usr/bin/torrentmanager
